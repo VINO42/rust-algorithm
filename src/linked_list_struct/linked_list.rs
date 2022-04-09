7ee0cba5a8ba6df907b9e6df24b19f8a89185b1d
wangbingya <38912428@qq.com>
2022-04-10 00:08:19 +0800

    todo: 链表

---
+++ src/linked_list_struct/linked_list.rs
@@ -0,0 +1,35 @@
+// 节点 连 接用 Box 指针 （ 大小确定 ）， 因 为 确 定 大 小 才 能 分 配 内 存
+
+type Link<T> = Option<Box<Node<T>>>;
+
+#[derive(Debug)]
+pub struct LinkedList<Node> {
+    // 前一个借点
+    pub pre: Link<T>,
+    // 后一个节点
+    pub next: Link<T>,
+    //链表长度
+    pub length: usize,
+}
+struct Node<T> {
+    element: T,
+    next: Link<T>,
+}
+impl<T> LinkedList<T> {
+    //构建一个链表
+    pub fn new() -> Self {
+        LinkedList {
+            pre: None,
+            next: None,
+            length: 0,
+        }
+    }
+    //是否为空
+    fn is_empty(&self) -> bool {
+        self.length == 0
+    }
+    //链表长度
+    fn length(&self) -> usize {
+        self.length
+    }
+}
---
+++ src/linked_list_struct/mod.rs
@@ -0,0 +1,4 @@
+pub mod linked_list;
+ 
+use linked_list::LinkedList;
+ 
