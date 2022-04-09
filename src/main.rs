e5195fcc2aae45416265c260c7374711f64d9554
wangbingya <38912428@qq.com>
2022-04-09 23:18:42 +0800

    stack

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use stack_struct::stack::Stack;
 
 fn main() {
     println!("Hello, world!");
-     let mut st=Stack::new();
+    let mut st = Stack::new();
     st.push(1);
     st.push(2);
-    println!("top:{},size:{}",st.top,st.data.len());
+    println!("top:{},size:{}", st.top, st.data.len());
     st.push(3);
-    println!("top:{},size:{}",st.top,st.data.len());
+    println!("top:{:?},size:{}", st.pop().unwrap(), st.data.len());
+    println!("top:{:?},size:{}", st.pop().unwrap(), st.data.len());
+    println!("top:{:?},size:{}", st.pop().unwrap(), st.data.len());
+    println!("isEmpty:{},size:{}", st.is_empty(), st.data.len());
+
 }
